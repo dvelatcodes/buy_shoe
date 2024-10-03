d09b0653435d490511ba6f7592101d69be2a959b
love olubummo <olubummolove3@gmail.com>
2024-10-03 12:29:18 +0100

    update: created main

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Button} from  './Button';
+export {default as Nav} from  './Nav';
+export {default as  Cart} from './Cart';
+export {default as Counter} from './Counter';
+export {default as Main} from  './Main';
