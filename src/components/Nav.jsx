8d88bc88266cc096e599ea23f6c661f6a6ac2efc
love olubummo <olubummolove3@gmail.com>
2024-10-04 14:32:53 +0100

    updated nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const constants = [
-  "Collections",
-  "Men",
-  "Women",
-  "About",
-  "Contact"
-]
+const constants = ["Collections", "Men", "Women", "About", "Contact"];
 
 const Nav = () => {
   return (
     <header>
-      <nav className='w-[80vw] m-auto h-[10vh] flex justify-between items-center'>
-        <img src="/images/logo.svg" alt="shoe logo" className='w-[200px] h-[30px]'/>
-        <section className='h-full w-[50vw] flex gap-12 items-center'>
-          {constants.map((data, i)=> <a href='#' key={i}>{data}</a>)}
+      <nav className="w-[80vw] m-auto h-[13vh] flex justify-between items-center border-b">
+        <img
+          src="/images/logo.svg"
+          alt="shoe logo"
+          className="w-[200px] h-[30px]"
+        />
+        <section className="h-full w-[50vw] flex gap-12">
+          {constants.map((data, i) => (
+            <a href="#" key={i} className="h-full font-light hover:border-b-4 flex items-center">
+              {data}
+            </a>
+          ))}
         </section>
-        <section className='flex items-center'>
-          <img src="/images/icon-cart.svg" alt="cart" className='mr-12'/>
-          <img src="/images/image-avatar.png" alt="profile" className='w-[65px] h-[60px]'/>
+        <section className="flex items-center">
+          <img src="/images/icon-cart.svg" alt="cart" className="mr-12" />
+          <img
+            src="/images/image-avatar.png"
+            alt="profile"
+            className="w-[65px] h-[60px]"
+          />
         </section>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
