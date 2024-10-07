dc4c09e7dea69822ffe59e85766e2a874d984c1f
love olubummo <olubummolove3@gmail.com>
2024-10-07 14:39:11 +0100

    update: edited nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,47 +1,82 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useStateContext } from "../utils/ContextProvider";
-
+import { IoCartOutline } from "react-icons/io5";
 
 const constants = ["Collections", "Men", "Women", "About", "Contact"];
 
 const Nav = () => {
   const [showHam, setShowHam] = useState(false);
 
-  const changeHam = ()=>{
-    setShowHam((prev)=> prev === false? true : false)
-  }
+  const changeHam = () => {
+    setShowHam((prev) => (prev === false ? true : false));
+  };
 
   const { attributes } = useStateContext();
 
   return (
-    <header className={`w-screen ${attributes.light === "light"? "bg-white" : "bg-black"}`}>
-      <nav className={`w-screen sm:w-[80vw] m-auto h-[13vh] flex justify-center sm:justify-between items-center border-b ${attributes.light === "light"? "border-slate-300" : "border-white"} relative`}>
-        <img src="/images/icon-menu.svg" alt="menu icon" className="sm:hidden w-[50] h-[50] cursor-pointer" onClick={changeHam}/>
+    <header
+      className={`w-screen ${
+        attributes.light === "light" ? "bg-white" : "bg-black"
+      }`}
+    >
+      <nav
+        className={`w-screen sm:w-[80vw] m-auto h-[13vh] flex justify-center sm:justify-between items-center border-b ${
+          attributes.light === "light" ? "border-slate-300" : "border-white"
+        } relative`}
+      >
+        <img
+          src="/images/icon-menu.svg"
+          alt="menu icon"
+          className="sm:hidden w-[50] h-[50] cursor-pointer"
+          onClick={changeHam}
+        />
 
-        <h1 className={`${attributes.light === "light"? "text-black" : "text-white"} text-2xl sm:text-4xl  font-semibold`}>sneakers</h1>
+        <h1
+          className={`${
+            attributes.light === "light" ? "text-black" : "text-white"
+          } text-2xl sm:text-4xl  font-semibold`}
+        >
+          sneakers
+        </h1>
 
-        <section className={`${attributes.light === "light"? "bg-white" : "bg-black"} ${showHam? "hidden" : "flex"} sm:bg-transparent h-screen sm:h-full w-[60%] sm:w-[50vw] flex gap-8 sm:gap-12 flex-col sm:flex-row absolute top-0 left-0 sm:relative z-40 pl-8 sm:pl-0 pt-6 sm:pt-0`}>
-
-          <img src="/images/icon-close.svg" alt="close icon" className="sm:hidden w-[20px] h-[20px] cursor-pointer mb-5" onClick={changeHam}/>
+        <section
+          className={`${
+            attributes.light === "light" ? "bg-white" : "bg-black"
+          } ${
+            showHam === false ? "hidden" : "flex"
+          } h-screen sm:h-full w-[60%] sm:w-[50vw] sm:flex gap-8 sm:gap-12 flex-col sm:flex-row absolute top-0 left-0 sm:relative z-40 pl-8 sm:pl-0 pt-6 sm:pt-0`}
+        >
+          <img
+            src="/images/icon-close.svg"
+            alt="close icon"
+            className="sm:hidden w-[20px] h-[20px] cursor-pointer mb-5"
+            onClick={changeHam}
+          />
           {constants.map((data, i) => (
             <a
               href="#"
               key={i}
-              className={`${attributes.light === "light"? "text-black" : "text-white"} w-fit sm:h-full font-bold sm:font-light hover:border-b-4 flex sm:items-center`}
+              className={`${
+                attributes.light === "light" ? "text-black" : "text-white"
+              } w-fit sm:h-full font-bold sm:font-light hover:border-b-4 flex sm:items-center`}
             >
               {data}
             </a>
           ))}
         </section>
         <section className="flex items-center">
-          <img src="/images/icon-cart.svg" alt="cart" className="mr-8 sm:mr-12" />
+          <IoCartOutline className={`mr-8 sm:mr-12 text-4xl ${attributes.light === "light" ? "text-black" : "text-white"}`} />
           <img
             src="/images/image-avatar.png"
             alt="profile"
             className="w-[50px] sm:w-[65px] h-[50px] sm:h-[60px] cursor-pointer profile"
           />
         </section>
-        <div className={`h-screen w-[40%] bg-black opacity-30  absolute right-0 top-0 ${showHam? "hidden" : "block"} sm:hidden`}></div>
+        <div
+          className={`h-screen w-[40%] bg-black opacity-30  absolute right-0 top-0 ${
+            showHam ===  false ? "hidden" : "block"
+          } sm:hidden`}
+        ></div>
       </nav>
     </header>
   );
